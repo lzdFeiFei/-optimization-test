ae7bf09a6ef1363bb799a3e40c0890c7a7f28993
lzdFeiFei <2802486551@qq.com>
2022-04-29 10:52:27 +0800

    cdn 优化

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>Axios test</div>
+  <div>Message: {{ message }}</div>
+  <el-button @click="getMessage">Click me</el-button>
+  <el-calendar v-model="value" />
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+const value = ref(new Date());
+axios.defaults.baseURL = "http://localhost:3000";
+
+const message = ref("");
+const getMessage = () => {
+  axios
+    .get("/")
+    .then(function (response) {
+      message.value = response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+</script>
+
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,44 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const CDN = {
+  css: ["https://unpkg.com/element-plus/dist/index.css"],
+  js: [
+    // "https://unpkg.com/vue@3/dist/vue.global.prod.js", // 生产环境
+    "https://unpkg.com/vue@next",
+    "https://unpkg.com/element-plus",
+    "https://unpkg.com/axios/dist/axios.min.js",
+    "https://unpkg.com/vue-router@4",
+    "https://unpkg.com/vuex@4",
+  ],
+};
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "": {
+        target: "http://localhost:3000/", //对应自己的接口
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
+  configureWebpack: (config) => {
+    return {
+      externals: {
+        devtool: "nosources-source-map",
+        vue: "Vue",
+        axios: "axios",
+        vuex: "Vuex",
+        "vue-router": "VueRouter",
+        "element-plus": "ElementPlus",
+      },
+    };
+  },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].cdn = CDN;
+      return args;
+    });
+  },
 });
